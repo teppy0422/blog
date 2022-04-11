c3d036de22bd39923fd8df62fa795cccbeee2433
teppy0422 <teppy@aol.jp>
2022-04-12 07:24:11 +0900

    タイトル変更

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h2>blog__________________</h2>
+    <h2>teppysBlog</h2>
     <Nuxt />
   </div>
 </template>
--- pages/_slug/index.vue
+++ pages/_slug/index.vue
@@ -2,9 +2,11 @@
   <main class="main">
     <h1 class="title">{{ title }}</h1>
     <p class="publishedAt">{{ publishedAt }}</p>
+    <p class="category">{{ category && category.name }}</p>
     <div class="post" v-html="body"></div>
   </main>
 </template>
+
 
 <script>
 import axios from "axios";
